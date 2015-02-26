6d888d2511b2c43dd07f43afe35d55f002c67d54
Stephania Thomas <sthomas@marketplace.org>
2015-02-26 15:19:18 -0800

    Index on each column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226185241) do
+ActiveRecord::Schema.define(version: 20150226231459) do
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.integer  "uuid"
