8bb4498ba78a9af881bde4b098b24775981fd702
Stephania Thomas <sthomas@marketplace.org>
2015-02-26 15:23:32 -0800

    Add relationship model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
 end
+
