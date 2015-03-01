8f25e2a5a14ccdfdf6415d147fd01cab39531895
Stephania Thomas <sthomas@marketplace.org>
2015-02-28 21:42:46 -0800

    Add new and show actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
   def index
     render json: User.all
   end
@@ -16,4 +35,11 @@ class UsersController < ApplicationController
     @users = @user.followers.paginate(page: params[:page])
     render json: @users
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
